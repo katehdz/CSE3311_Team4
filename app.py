3b94c9b3caa71064d7dfc6d0bcc4dc69eb572f7e
MohammadHR10 <mhr9808@mavs.uta.edu>
2025-09-22 19:54:19 -0500

    fixed bug

--- app.py
+++ app.py
@@ -359,4 +359,4 @@ def internal_error(error):
 
 if __name__ == '__main__':
     debug_mode = os.getenv('FLASK_DEBUG', 'False').lower() == 'true'
-    app.run(debug=debug_mode, host='0.0.0.0', port=5000)
+    app.run(debug=debug_mode, host='0.0.0.0', port=5001)
